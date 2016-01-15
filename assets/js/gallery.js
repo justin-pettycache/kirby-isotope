41a2f41fd9357d03a6ed9eea9fddd9477a3b2887
justin-pettycache <justin@pettycache.info>
2016-01-15 16:08:32 +0800

    URL Hash Function

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,19 +1,36 @@
-$(document).ready(function() {
-  var $container = $('.gallery').isotope({
-    itemSelector: '.gallery-item'
+function getHashFilter() {
+  var matches = location.hash.match(/filter=([^&]+)/i);
+  var hashFilter = matches && matches[1];
+  return hashFilter && decodeURIComponent(hashFilter);
+}
+
+$().ready(function() {
+  var $container = $('.gallery');
+  var $buttonGroup = $('.gallery-filters');
+
+  $buttonGroup.on('click', 'button', function() {
+    var filterAttr = $(this).attr('data-filter');
+    location.hash = 'filter=' + encodeURIComponent(filterAttr);
   });
 
-  $('.gallery-filters').on('click', 'button', function() {
-    var filterValue = $(this).attr('data-filter');
-    $container.isotope({filter: filterValue});
-  });
-  
-  $('.gallery-filters').each(function(i, buttonGroup) {
-    var $buttonGroup = $(buttonGroup);
-    $buttonGroup.on('click', 'button', function() {
-      $buttonGroup.find('.active').removeClass('active');
-      $(this).addClass('active');
+  var isIsotopeInit = false;
+
+  function onHashchange() {
+    var hashFilter = getHashFilter();
+    if(!hashFilter && isIsotopeInit) {
+      return;
+    }
+    isIsotopeInit = true;
+    $container.isotope({
+      itemSelector: '.gallery-item',
+      filter: hashFilter
     });
-  });
-  
+    if(hashFilter) {
+      $buttonGroup.find('.active').removeClass('active');
+      $buttonGroup.find('[data-filter="' + hashFilter + '"]').addClass('active');
+    }
+  }
+
+  $(window).on('hashchange', onHashchange);
+  onHashchange();
 });
